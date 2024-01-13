f7ddb247d22594485b293edc7bc3a2ca3c037f3f
Eduardo Poot <eduardopoot_@outlook.com>
2024-01-12 23:09:26 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const toggle = document.querySelector('.toggle__circle')
+
+toggle.addEventListener('click', e => {
+    toggle.classList.toggle('toggle__active')
+
+
+    if(!toggle.classList.value.includes('toggle__active')) {
+        anuallyHTML() 
+        return
+    }
+
+    monthHTML()
+})
+
+function calcAnually() {
+    const basicPrice = (19.999 * 10).toFixed(2)
+    const professionalPrice = (24.999 * 10).toFixed(2)
+    const masterPrice = (39.999 * 10).toFixed(2)
+
+    return {
+        basicPrice,
+        professionalPrice,
+        masterPrice
+    }
+}
+
+const basic = document.querySelector('#basic')
+const professional = document.querySelector('#professional')
+const master = document.querySelector('#master')
+
+function anuallyHTML() {
+    const {basicPrice, professionalPrice, masterPrice} = calcAnually()
+
+    basic.innerHTML = `&dollar;${basicPrice}`
+    professional.innerHTML = `&dollar;${professionalPrice}`
+    master.innerHTML = `&dollar;${masterPrice}`
+
+    console.log(basic)
+}
+
+function monthHTML() {
+    basic.innerHTML = `&dollar;19.99`
+    professional.innerHTML = `&dollar;24.99`
+    master.innerHTML = `&dollar;39.99`
+}
